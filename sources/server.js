94b6d7fb881606d3fc416bfb5e6c515887cef41b
actmember <actmember@actmembers-iMac.local>
2019-01-02 20:39:26 +0900

    add express module and test router

--- sources/server.js
+++ sources/server.js
@@ -1,10 +1,19 @@
-var http = require('http');
+const express = require('express');
+const app = express();
 
-var handleRequest = function(request, response) {
-  console.log('Received request for URL: ' + request.url);
-  response.writeHead(200);
-  response.end('Hello World 1.0!');
-}
+app.get('/', (request, response) => response.send('Hello world'));
 
-var www = http.createServer(handleRequest);
-www.listen(8080);
+// parameter: 검색어와 이미지 파일을 받아서 text detection
+// return: 4개의 꼭지점 좌표 -> 이것을 이용해서 화면에서 그린다.
+app.post('/locations', (request, response) => {
+
+    const vertices = new Array(4);
+    vertices[0] = "{1, 1}";
+    vertices[1] = "{2, 2}";
+    vertices[2] = "{3, 3}";
+    vertices[3] = "{4, 4}";
+
+    response.send(vertices);
+});
+
+app.listen(3000, () => console.log('App listening on port 3000'));
